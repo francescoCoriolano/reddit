b07e23efa61d31f03c6abe5654b8cac3e44ed4ec
Francesco Coriolano <francesco.coriolano@studiographene.com>
2025-03-25 08:56:01 +0000

    added react router to the application

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <div className="text-2xl font-semibold">Login Page</div>
+    </div>
+  );
+}
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div className="flex flex-col min-h-screen justify-center items-center">
+      Login Page
+    </div>
+  );
+};
+
+export default LoginPage;
